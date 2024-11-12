334e4318055f2c5786be930e946627599b369cd0
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2024-11-12 15:31:11 +0100

    Powershell C:\Users\pfp\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 updated -> Theme changed

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,2 @@
 oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/1_shell.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/sim-web.omp.json" | Invoke-Expression
